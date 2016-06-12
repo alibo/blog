5c42aeeff8b1796452cd38e80019ea3800520ecb
Ali Borhani <aliborhani1@gmail.com>
2016-06-13 02:36:11 +0430

    add elixir

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp');
+var elixir = require('laravel-elixir');
+var argv = require('yargs').argv;
+
+elixir.config.assetsPath = 'content/resources';
+elixir.config.publicPath = 'content/assets';
+
+elixir(function(mix) {
+    var env = argv.e || argv.env || 'default';
+
+    mix.styles([
+	    'prism.css',
+		'poole.css',
+		'syntax.css',
+		'lanyon.css',
+	])
+	.scripts([
+		'prism.js'
+	])
+    .exec('php katana build --env=' + env, [
+    	'./content/*', 
+    	'./content/**/*', 
+    	'./config*.php'
+    ])
+    .browserSync({
+        server: { baseDir: 'public' },
+        proxy: null,
+        files: [ 'public/**/*' ]
+    });
+});
