e8b97cb402de249013e8b08ace28a3c7043ba18e
Ali Borhani <aliborhani1@gmail.com>
2016-08-14 21:41:47 +0430

    improve rss

--- content/rss.blade.php
+++ content/rss.blade.php
@@ -1,9 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
-<feed xmlns="http://www.w3.org/2005/Atom">
+<feed xmlns="http://www.w3.org/2005/Atom"
+  xmlns:thr="http://purl.org/syndication/thread/1.0"
+  xml:lang="fa-IR">
 
  <title>{{ $siteName }}</title>
- <link href="http://borhani.pw/feed.rss" rel="self"/>
- <link href="http://borhani.pw/"/>
+ <link href="http://borhani.pw/feed.rss" type="application/atom+xml" rel="self"/>
+ <link href="http://borhani.pw/" rel="alternate" type="text/html" />
  <id>https://borhani.pw</id>
 
 @foreach($blogPosts as $post)
@@ -12,7 +14,7 @@
    <link href="https://borhani.pw{{ $post->path }}"/>
    <updated>{{ DateTime::createFromFormat('U', $post->date)->format(DATE_ATOM) }}</updated>
    <id>https://borhani.pw{{ $post->path }}</id>
-   <content type="html">![CDATA[{!! $post->body !!}]]</content>
+   <content type="html">{{ $post->body }}</content>
  </entry>
 @endforeach
 
